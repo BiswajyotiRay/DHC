32316f9fee25d1f90c00af6ec22e2e572edffcc7
Biswajyoti Ray <106693738+BiswajyotiRay@users.noreply.github.com>
2025-03-17 03:23:30 +0530

    Update evaluate_Ntimes.py

--- code/evaluate_Ntimes.py
+++ code/evaluate_Ntimes.py
@@ -3,118 +3,133 @@ import numpy as np
 import argparse
 from medpy import metric
 from tqdm import tqdm
+import torch
+import torch.nn.functional as F
 
+from utils import read_list, read_nifti
+from utils.config import Config
+
+# Argument Parsing
 parser = argparse.ArgumentParser()
 parser.add_argument('--task', type=str, default='synapse')
-parser.add_argument('--exp', type=str, default="fully")
+parser.add_argument('--exp', type=str, default="unetrpp")
 parser.add_argument('--folds', type=int, default=3)
 parser.add_argument('--cps', type=str, default=None)
 args = parser.parse_args()
 
-from utils import read_list, read_nifti
-import torch
-import torch.nn.functional as F
-from utils.config import Config
+# Load configuration for dataset
 config = Config(args.task)
 
 if __name__ == '__main__':
-
-
     ids_list = read_list('test', task=args.task)
     results_all_folds = []
-    txt_path = "./logs/"+args.exp+"/evaluation_res.txt"
+    txt_path = f"./logs/{args.exp}/evaluation_res.txt"
+
     print("\n Evaluating...")
-    fw = open(txt_path, 'w')
-    for fold in range(1, args.folds+1):
+    with open(txt_path, 'w') as fw:
+        for fold in range(1, args.folds + 1):
+            test_cls = [i for i in range(1, config.num_cls)]
+            values = np.zeros((len(ids_list), len(test_cls), 2))  # Dice and ASD
 
-        test_cls = [i for i in range(1, config.num_cls)]
-        values = np.zeros((len(ids_list), len(test_cls), 2)) # dice and asd
+            for idx, data_id in enumerate(tqdm(ids_list)):
+                pred_path = os.path.join("./logs", args.exp, f"fold{fold}", f"predictions_{args.cps}", f"{data_id}.nii.gz")
+                pred = read_nifti(pred_path)
 
-        for idx, data_id in enumerate(tqdm(ids_list)):
-            pred = read_nifti(os.path.join("./logs",args.exp, "fold"+str(fold), "predictions_"+str(args.cps),f'{data_id}.nii.gz'))
-            if args.task == "amos":
-                label = read_nifti(os.path.join(config.base_dir, 'labelsVa', f'{data_id}.nii.gz'))
-            else:
-                label = read_nifti(os.path.join(config.base_dir, 'labelsTr', f'label{data_id}.nii.gz'))
-            label = label.astype(np.int8)
-            dd, ww, hh = label.shape
-            label = torch.FloatTensor(label).unsqueeze(0).unsqueeze(0)
+                # Load Ground Truth
+                if args.task == "amos":
+                    label_path = os.path.join(config.base_dir, 'labelsVa', f'{data_id}.nii.gz')
+                else:
+                    label_path = os.path.join(config.base_dir, 'labelsTr', f'label{data_id}.nii.gz')
 
+                label = read_nifti(label_path).astype(np.int8)
 
-            if args.task == "amos":
-                resize_shape = (config.patch_size[0] + config.patch_size[0] // 8,
-                                config.patch_size[1] + config.patch_size[1] // 8,
-                                config.patch_size[2] + config.patch_size[2] // 8)
-                label = F.interpolate(label, size=resize_shape,mode='nearest')
-            else:
-                label = F.interpolate(label, size=(dd, ww//2, hh//2),mode='nearest')
-            label = label.squeeze().numpy()
+                # Check if dimensions match before interpolation
+                dd, ww, hh = label.shape
+                pred_shape = pred.shape
 
-            for i in test_cls:
-                pred_i = (pred == i)
-                label_i = (label == i)
-                if pred_i.sum() > 0 and label_i.sum() > 0:
-                    dice = metric.binary.dc(pred == i, label == i) * 100
-                    hd95 = metric.binary.asd(pred == i, label == i)
-                    values[idx][i-1] = np.array([dice, hd95])
-                elif pred_i.sum() > 0 and label_i.sum() == 0:
-                    dice, hd95 = 0, 128
-                elif pred_i.sum() == 0 and label_i.sum() > 0:
-                    dice, hd95 =  0, 128
-                elif pred_i.sum() == 0 and label_i.sum() == 0:
-                    dice, hd95 =  1, 0
+                if pred_shape != (dd, ww, hh):
+                    label = torch.FloatTensor(label).unsqueeze(0).unsqueeze(0)
 
-                values[idx][i-1] = np.array([dice, hd95])
+                    # Resizing logic to match prediction
+                    if args.task == "amos":
+                        resize_shape = (
+                            config.patch_size[0] + config.patch_size[0] // 8,
+                            config.patch_size[1] + config.patch_size[1] // 8,
+                            config.patch_size[2] + config.patch_size[2] // 8
+                        )
+                        label = F.interpolate(label, size=resize_shape, mode='nearest')
+                    else:
+                        label = F.interpolate(label, size=pred_shape, mode='nearest')
 
-        values_mean_cases = np.mean(values, axis=0)
-        results_all_folds.append(values)
-        fw.write("Fold" + str(fold) + '\n')
-        fw.write("------ Dice ------" + '\n')
-        fw.write(str(np.round(values_mean_cases[:,0],1)) + '\n')
-        fw.write("------ ASD ------" + '\n')
-        fw.write(str(np.round(values_mean_cases[:,1],1)) + '\n')
-        fw.write('Average Dice:'+str(np.mean(values_mean_cases, axis=0)[0]) + '\n')
-        fw.write('Average  ASD:'+str(np.mean(values_mean_cases, axis=0)[1]) + '\n')
-        fw.write("=================================")
-        print("Fold", fold)
-        print("------ Dice ------")
-        print(np.round(values_mean_cases[:,0],1))
-        print("------ ASD ------")
-        print(np.round(values_mean_cases[:,1],1))
-        print(np.mean(values_mean_cases, axis=0)[0], np.mean(values_mean_cases, axis=0)[1])
+                    label = label.squeeze().numpy()
 
-    results_all_folds = np.array(results_all_folds)
+                # Compute Metrics
+                for i in test_cls:
+                    pred_i = (pred == i)
+                    label_i = (label == i)
 
-    fw.write('\n\n\n')
-    fw.write('All folds' + '\n')
+                    if pred_i.sum() > 0 and label_i.sum() > 0:
+                        dice = metric.binary.dc(pred_i, label_i) * 100
+                        hd95 = metric.binary.hd95(pred_i, label_i)
+                    elif pred_i.sum() > 0 and label_i.sum() == 0:
+                        dice, hd95 = 0, 128
+                    elif pred_i.sum() == 0 and label_i.sum() > 0:
+                        dice, hd95 = 0, 128
+                    elif pred_i.sum() == 0 and label_i.sum() == 0:
+                        dice, hd95 = 1, 0
 
-    results_folds_mean = results_all_folds.mean(0)
+                    values[idx][i - 1] = np.array([dice, hd95])
 
-    for i in range(results_folds_mean.shape[0]):
-        fw.write("="*5 + " Case-" + str(ids_list[i]) + '\n')
-        fw.write('\tDice:'+str(np.round(results_folds_mean[i][:,0],2).tolist()) + '\n')
-        fw.write('\t ASD:'+str(np.round(results_folds_mean[i][:,1],2).tolist()) + '\n')
-        fw.write('\t'+'Average Dice:'+str(np.mean(results_folds_mean[i], axis=0)[0]) + '\n')
-        fw.write('\t'+'Average  ASD:'+str(np.mean(results_folds_mean[i], axis=0)[1]) + '\n')
+            # Compute Mean Values
+            values_mean_cases = np.mean(values, axis=0)
+            results_all_folds.append(values)
 
-    fw.write("=================================\n")
-    fw.write('Final Dice of each class\n')
-    fw.write(str([round(x,1) for x in results_folds_mean.mean(0)[:,0].tolist()]) + '\n')
-    fw.write('Final ASD of each class\n')
-    fw.write(str([round(x,1) for x in results_folds_mean.mean(0)[:,1].tolist()]) + '\n')
-    print("=================================")
-    print('Final Dice of each class')
-    print(str([round(x,1) for x in results_folds_mean.mean(0)[:,0].tolist()]))
-    print('Final ASD of each class')
-    print(str([round(x,1) for x in results_folds_mean.mean(0)[:,1].tolist()]))
-    std_dice = np.std(results_all_folds.mean(1).mean(1)[:,0])
-    std_hd = np.std(results_all_folds.mean(1).mean(1)[:,1])
+            fw.write(f"Fold {fold}\n")
+            fw.write("------ Dice ------\n")
+            fw.write(str(np.round(values_mean_cases[:, 0], 1)) + '\n')
+            fw.write("------ ASD ------\n")
+            fw.write(str(np.round(values_mean_cases[:, 1], 1)) + '\n')
+            fw.write(f'Average Dice: {np.mean(values_mean_cases, axis=0)[0]}\n')
+            fw.write(f'Average ASD: {np.mean(values_mean_cases, axis=0)[1]}\n')
+            fw.write("=================================\n")
 
-    fw.write('Final Avg Dice: '+str(round(results_folds_mean.mean(0).mean(0)[0], 2)) +'±' +  str(round(std_dice,2)) + '\n')
-    fw.write('Final Avg  ASD: '+str(round(results_folds_mean.mean(0).mean(0)[1], 2)) +'±' +  str(round(std_hd,2)) + '\n')
+            print(f"Fold {fold}")
+            print("------ Dice ------")
+            print(np.round(values_mean_cases[:, 0], 1))
+            print("------ ASD ------")
+            print(np.round(values_mean_cases[:, 1], 1))
+            print(np.mean(values_mean_cases, axis=0)[0], np.mean(values_mean_cases, axis=0)[1])
 
-    print('Final Avg Dice: '+str(round(results_folds_mean.mean(0).mean(0)[0], 2)) +'±' +  str(round(std_dice,2)))
-    print('Final Avg  ASD: '+str(round(results_folds_mean.mean(0).mean(0)[1], 2)) +'±' +  str(round(std_hd,2)))
+        # Aggregate Results Across All Folds
+        results_all_folds = np.array(results_all_folds)
+        results_folds_mean = results_all_folds.mean(0)
 
+        fw.write('\n\n\nAll folds\n')
+        for i in range(results_folds_mean.shape[0]):
+            fw.write("=" * 5 + f" Case-{ids_list[i]}\n")
+            fw.write(f'\tDice: {str(np.round(results_folds_mean[i][:, 0], 2).tolist())}\n')
+            fw.write(f'\t ASD: {str(np.round(results_folds_mean[i][:, 1], 2).tolist())}\n')
+            fw.write(f'\tAverage Dice: {np.mean(results_folds_mean[i], axis=0)[0]}\n')
+            fw.write(f'\tAverage ASD: {np.mean(results_folds_mean[i], axis=0)[1]}\n')
 
+        # Final Results
+        fw.write("=================================\n")
+        fw.write("Final Dice of each class\n")
+        fw.write(str([round(x, 1) for x in results_folds_mean.mean(0)[:, 0].tolist()]) + '\n')
+        fw.write("Final ASD of each class\n")
+        fw.write(str([round(x, 1) for x in results_folds_mean.mean(0)[:, 1].tolist()]) + '\n')
 
+        print("=================================")
+        print("Final Dice of each class")
+        print(str([round(x, 1) for x in results_folds_mean.mean(0)[:, 0].tolist()]))
+        print("Final ASD of each class")
+        print(str([round(x, 1) for x in results_folds_mean.mean(0)[:, 1].tolist()]))
+
+        std_dice = np.std(results_all_folds.mean(1).mean(1)[:, 0])
+        std_hd = np.std(results_all_folds.mean(1).mean(1)[:, 1])
+
+        fw.write(f'Final Avg Dice: {round(results_folds_mean.mean(0).mean(0)[0], 2)} ± {round(std_dice, 2)}\n')
+        fw.write(f'Final Avg ASD: {round(results_folds_mean.mean(0).mean(0)[1], 2)} ± {round(std_hd, 2)}\n')
+
+        print(f'Final Avg Dice: {round(results_folds_mean.mean(0).mean(0)[0], 2)} ± {round(std_dice, 2)}')
+        print(f'Final Avg ASD: {round(results_folds_mean.mean(0).mean(0)[1], 2)} ± {round(std_hd, 2)}')
